c9e05a7d64ef99a0cd7bbf3181d80eb5a7b32cd2
dhanush <dhanushpk50@gmail.com>
2024-12-22 12:38:15 +0530

    cicd pipeline

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -49,6 +49,8 @@ router.post('/', async (req, res) => {
     if (error) {
         return res.status(500).json({ error: error.message });
     }
+
+    return res.status(200).json({ message: "Customer added successfully" });
 });
 
 module.exports = router;
